dbe5f164d33d877498b4ecd4188261b43f9729a8
MD FARHAN <50393822+mdfarhan0203@users.noreply.github.com>
2024-02-15 20:06:27 +0530

    added optinal chaining for better way to handle error

--- src/component/MovieListing/MovieListing.js
+++ src/component/MovieListing/MovieListing.js
@@ -18,20 +18,22 @@ const MovieListing = () => {
         <h2>MOVIES</h2>
       </div>
       <div className="listing-movies">
-        {movies.Error ? <h2>{movies.Error}</h2>:movies.Search &&
+        {movies?.Error ? <h2>{movies.Error}</h2>:(movies.Search &&
           movies.Search.map((item) => {
             return <MovieCard item={item} key={item.imdbID} />;
-          })}
+          }))
+          }
       </div>
       {/* Series List */}
       <div className="series-title">
         <h2>SERIES</h2>
       </div>
       <div className="listing-series">
-        {series.Error ? <h2>{series.Error}</h2>:series.Search &&
+        {series?.Error ? <h2>{series.Error}</h2>:(series.Search &&
           series.Search.map((item) => {
             return <MovieCard item={item} key={item.imdbID} />;
-          })}
+          }))
+          }
       </div>
     </div>
   );
